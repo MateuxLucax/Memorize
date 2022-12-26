1017bb2eed320193261b78931a483a5c7a37c9d6
Mateus Lucas <mateuxlucax@gmail.com>
2022-12-26 19:39:26 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mateus Brandt on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
